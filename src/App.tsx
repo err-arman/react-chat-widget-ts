b84081f5de85bc17393ee468dbcfec58957e1720
yousuf <gourabxz@gmail.com>
2023-01-04 16:04:56 +0600

    initialize redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import '@/styles/test.scss'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment } from 'redux/slices/counterSlice'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useSelector((state: any) => state.counter.value)
+  const dispatch = useDispatch()
 
   return (
     <div className="App">
@@ -18,7 +21,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => dispatch(increment())}>
           count is {count}
         </button>
         <p>
