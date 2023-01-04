2e6a988b6f65f92f32e2d23b26cc631d19a2f334
yousuf <gourabxz@gmail.com>
2023-01-04 15:01:32 +0600

    initialize project with sass and ts basepath

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import '@/styles/test.scss'
 
 function App() {
   const [count, setCount] = useState(0)
