89c6e0dfb870055d254dd0eb76f25120b8b17da1
yousuf <gourabxz@gmail.com>
2023-01-05 16:06:12 +0600

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,6 @@ cd dist
 # place .nojekyll to bypass Jekyll processing
 echo > .nojekyll
 
-git init
-git checkout -B deployment
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:Yousuf-Basir/react-chat-widget-ts.git deployment:gh-pages
+git push -f git@github.com:Yousuf-Basir/react-chat-widget-ts.git main:gh-pages
 
 cd -
